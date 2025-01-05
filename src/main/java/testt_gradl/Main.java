20ce568c63002345e3df42f9611ddb820faefaac
anthony <anthony.abs.bousleiman@gmail.com>
2025-01-06 00:15:25 +0200

    ana ghiyaret

--- src/main/java/testt_gradl/Main.java
+++ src/main/java/testt_gradl/Main.java
@@ -2,6 +2,6 @@ package testt_gradl;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Worldyggff!");
+        System.out.println("Hello, Worldy bbff!");
     }
 }
